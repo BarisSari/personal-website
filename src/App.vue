95085f979ec21fb1276fc3febde6c39c2872de43
Baris Sari <sariba@itu.edu.tr>
2020-10-09 12:40:08 +0300

    Improve styling, try to solve not shown error

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div id="app">
-    <Main/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-
-import Main from "./views/Main";
-
 export default {
   name: 'App',
-  components: {
-    Main,
-  }
 }
 </script>
 
