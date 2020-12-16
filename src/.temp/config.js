88cddfb8b54d9838acdfcab6d68ac706f981233f
Baris Sari <sariba@itu.edu.tr>
2020-12-16 11:21:40 +0300

    remove .temp directory

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - barissari.com",
-  "siteUrl": "barissari.com",
-  "version": "0.7.23",
-  "catchLinks": true
-}
